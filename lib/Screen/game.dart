6f1f9fa9c3b87031369eac834644abd612ce6489
aleena02122002 <khananna919@gmail.com>
2024-03-28 21:20:05 +0500

    DART code of tic-tac toe

--- lib/Screen/game.dart
+++ lib/Screen/game.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 class GameView extends StatefulWidget {
@@ -14,10 +15,105 @@ class _GameViewState extends State<GameView> {
   late String _winner;
   late bool _gameOver;
   @override
+  void initState() {
+    super.initState();
+    _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+    _currentPlayer = "X";
+    _winner = "";
+    _gameOver = false;
+  }
 
+  void _resetGame() {
+    setState(() {
+      _board = List.generate(3, (_) => List.generate(3, (_) => ""));
+      _currentPlayer = "X";
+      _winner = "";
+      _gameOver = false;
+    });
+  }
+
+  void _makeMove(int row, int col) {
+    if (_board[row][col] != "" || _gameOver) {
+      return;
+    }
+    setState(() {
+      _board[row][col] = _currentPlayer;
+      if (_board[row][0] == _currentPlayer &&
+          _board[row][1] == _currentPlayer &&
+          _board[row][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][col] == _currentPlayer &&
+          _board[1][col] == _currentPlayer &&
+          _board[2][col] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][0] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][2] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      } else if (_board[0][2] == _currentPlayer &&
+          _board[1][1] == _currentPlayer &&
+          _board[2][0] == _currentPlayer) {
+        _winner = _currentPlayer;
+        _gameOver = true;
+      }
+      _currentPlayer = _currentPlayer == "X" ? "O" : "X";
+
+      if (!_board.any((row) => row.any((cell) => cell == ""))) {
+        _gameOver = true;
+        _winner = "It's a Tie";
+      }
+      if (_winner != "") {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.success,
+          animType: AnimType.rightSlide,
+          btnOkText: "Play Again",
+          title: _winner == "X"
+              ? widget.player1 + "Won!"
+              : _winner == "O"
+                  ? widget.player2 + "Won!"
+                  : "It's a Tie",
+          btnOkOnPress: (){
+            _resetGame();
+          },
+        )
+            ..show();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(27, 51, 118, 80),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(height: 70),
+          SizedBox(
+            height: 120,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Turn: ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],),
+      ),
+    );
   }
 }
